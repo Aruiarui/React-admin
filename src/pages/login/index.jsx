4b7f33ad6b1733f89316b7f1be6dc43e1d6a981f
yueban <yueban@qq.com>
2019-04-01 17:02:22 +0800

    实现login界面用户登录校验功能

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -6,8 +6,33 @@ import {
 import logo from './logo.png'
 import './index.less'
 
-export default class Login extends Component {
+@Form.create()
+class Login extends Component {
+  login = (e) => {
+    e.preventDefault();
+  }
+
+  validator = (rule, value, callback) => {
+
+    const length = value && value.length;
+    const pwdReg = /^[a-zA-Z0-9_]+$/;
+
+    if(!value) {
+      callback('必须输入密码')
+    }else if(length<4) {
+      callback('密码必须大于4位')
+    }else if(length>10) {
+      callback('密码必须小于10位')
+    }else if(!pwdReg.test(value)) {
+      callback('用户名必须是由字母、数字、下划线组成')
+    }else {
+      callback();
+    }
+  }
+
+
   render() {
+    const {getFieldDecorator} = this.props.form;
     return (
       <div className='login'>
         <header className='login-header'>
@@ -18,10 +43,29 @@ export default class Login extends Component {
           <h3>用户登录</h3>
           <Form onSubmit={this.handleSubmit} className="login-form">
             <Form.Item>
-              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名" />
+              {getFieldDecorator('userName',{
+                rules:[
+                  {required:true, message:'必须输入用户名', whitespace:true},
+                  {min:4, message:'用户名必须大于4位'},
+                  {max:10, message:'用户名必须小于10位'},
+                  {pattern:/^[a-zA-Z0-9_]+$/, message:'用户名必须是由字母、数字、下划线组成'}],
+              })(
+                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名" />
+              )}
             </Form.Item>
             <Form.Item>
-              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="密码" />
+            {getFieldDecorator('passWord',{
+                rules:[
+                  {validator: this.validator}
+                  // {required:true, message:'必须输入密码', whitespace:true},
+                  // {min:4, message:'密码必须大于4位'},
+                  // {max:4, message:'密码必须小于10位'},
+                  // {pattern:/^[a-zA-Z0-9_]+$/, message:'密码必须是由字母、数字、下划线组成'}
+                ],
+              })(
+                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="密码" />
+              )}
+              
             </Form.Item>
             <Form.Item>
               <Button type="primary" htmlType="submit" className="login-form-button">
@@ -36,3 +80,4 @@ export default class Login extends Component {
     )
   }
 }
+export default Login;
